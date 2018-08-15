4b85fd17d6001582cd6e15ed9a9277ee3a486c93
minchao.zhu <readthinkcommunicate@gmail.com>
2018-08-15 22:03:10 +0100

    week_2 Interview Question. EdgeInMSTJudge

--- src/week_2/MinimumSpanningTree/InterviewQuestion/EdgeInMSTJudge.java
+++ src/week_2/MinimumSpanningTree/InterviewQuestion/EdgeInMSTJudge.java
@@ -8,11 +8,15 @@ import edu.princeton.cs.algs4.SET;
  * Is an edge in a MST. Given an edge-weighted graph GG and an edge ee, design a linear-time algorithm to determine whether ee appears in some MST of GG.
  *
  * Note: Since your algorithm must take linear time in the worst case, you cannot afford to compute the MST itself.
+ *
  */
+
+//TODO: Note，it is possible that this Graph is splited into two or more separated graphs. So it is possible to have minimum spanning FOREST.
 public class EdgeInMSTJudge {
 
     private EdgeWeightedGraph GG;
     private Edge ee;
+
     public EdgeInMSTJudge(EdgeWeightedGraph GG, Edge ee){
         this.GG = GG;
         this.ee = ee;
@@ -21,7 +25,10 @@ public class EdgeInMSTJudge {
     private boolean isEdgeInMST(EdgeWeightedGraph G, Edge e){
         //This algorithm is good enough. I think no need to check if this graph is acyclic.
         SET<Integer> vertices = new SET<Integer>();
+
         double weight = e.weight();
+
+
         for (Edge edge: G.edges()) {
             if (edge.weight() < weight) { // So edge e's two vertices will enter to SET if there's less weighted edge between them.
                 int v = edge.either();
